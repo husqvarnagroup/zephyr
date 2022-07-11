0179820591ef13b2fa44c86352ae4d39da7e3ae0
Marc Lasch <marc.lasch@husqvarnagroup.com>
2022-07-11 11:57:39 +0200

    [sg toup] mgmt/mcumgr: Fix format specifier
    Remove build warnings when building for `native_posix_64`.

--- subsys/mgmt/mcumgr/smp_bt.c
+++ subsys/mgmt/mcumgr/smp_bt.c
@@ -236,8 +236,8 @@ static ssize_t smp_bt_chr_write(struct bt_conn *conn,
 	}
 
 	if (net_buf_tailroom(nb) < len) {
-		LOG_DBG("SMP packet len (%zu) > net_buf len (%zu)",
-			len, net_buf_tailroom(nb));
+		LOG_DBG("SMP packet len (%" PRIu16 ") > net_buf len (%zu)", len,
+			net_buf_tailroom(nb));
 		mcumgr_buf_free(nb);
 		return BT_GATT_ERR(BT_ATT_ERR_INSUFFICIENT_RESOURCES);
 	}
