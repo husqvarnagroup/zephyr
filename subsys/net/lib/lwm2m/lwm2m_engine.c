1c5e61f58ed2f6f10860b8d28904f831cbf7f624
Alexey Nezhdanov <alexey.nezhdanov@husqvarnagroup.com>
2022-08-26 10:35:44 +0200

    [sg toup] lwm2m: bugfix: don't mess up the count of updated objects.

--- subsys/net/lib/lwm2m/lwm2m_engine.c
+++ subsys/net/lib/lwm2m/lwm2m_engine.c
@@ -604,10 +604,11 @@ int lwm2m_notify_observer_path(struct lwm2m_obj_path *path)
 		SYS_SLIST_FOR_EACH_CONTAINER(&sock_ctx[i]->observer, obs, node) {
 			if (lwm2m_notify_observer_list(&obs->path_list, path)) {
 				/* update the event time for this observer */
-				ret = engine_observe_attribute_list_get(&obs->path_list, &nattrs,
-								    sock_ctx[i]->srv_obj_inst);
-				if (ret < 0) {
-					return ret;
+				int rc = engine_observe_attribute_list_get(&obs->path_list, &nattrs,
+									sock_ctx[i]->srv_obj_inst);
+
+				if (rc < 0) {
+					return rc;
 				}
 
 				if (nattrs.pmin) {
