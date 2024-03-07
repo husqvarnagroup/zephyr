a171f29480f293089912dbabf9e7aa49a3708cf2
Andreas Müller <andreas.mueller@husqvarnagroup.com>
2024-03-07 18:40:02 +0100

    [sg noup] ppp: yield in PPP driver
    On the Zephyr RM gateway, when we receive a packet via Lemonbeat and it is
    sent over PPP, this previously blocked the lb_radio driver from sending a
    MAC ACK (this is independent of thread priorities if the PPP code never
    yields).
    
    This was previously addressed by a k_sleep(K_MSEC(50)) before handing over
    new packets from Lemonbeat to PPP in sg_ppp_bridge_process_pkt(), but
    yielding in ppp_send_flush() seems cleaner.
    
    Issue regarding upstreaming:
    https://github.com/zephyrproject-rtos/zephyr/issues/70696
    
    Summary: Zephyr does not want this implementation upstreamed; suggestion
    is to rewrite PPP to use the interrupt-driver UART API. If/when this is
    done, we can potentially switch and drop this patch.
    
    (cherry picked from commit 1d77de7f50c4201f7ba491a45cee417bef8ee9f6)
    (squashed commit 3a0993d7c153f017a8a72932f99d9b1c6332ebf6)

--- drivers/net/ppp.c
+++ drivers/net/ppp.c
@@ -39,6 +39,17 @@ LOG_MODULE_REGISTER(net_ppp, LOG_LEVEL);
 #define UART_BUF_LEN CONFIG_NET_PPP_UART_BUF_LEN
 #define UART_TX_BUF_LEN CONFIG_NET_PPP_ASYNC_UART_TX_BUF_LEN
 
+/* yield control every N bytes so that higher-priority threads can run
+ * (note: N=28 should be about 2 ms at 115200 bps)
+ */
+#define UART_YIELD_INTERVAL_BYTES 28
+
+#if DT_NODE_HAS_PROP(DT_CHOSEN(zephyr_ppp_uart), current_speed)
+BUILD_ASSERT(UART_YIELD_INTERVAL_BYTES * 8 * 1000 /
+			     DT_PROP(DT_CHOSEN(zephyr_ppp_uart), current_speed) <= 5,
+	     "current UART k_yield() interval & speed leads to blocking time > 5 ms");
+#endif
+
 enum ppp_driver_state {
 	STATE_HDLC_FRAME_START,
 	STATE_HDLC_FRAME_ADDRESS,
@@ -403,6 +414,12 @@ static int ppp_send_flush(struct ppp_driver_context *ppp, int off)
 	} else {
 		while (off--) {
 			uart_poll_out(ppp->dev, *buf++);
+			static uint8_t counter = 0;
+			counter++;
+			if (counter % UART_YIELD_INTERVAL_BYTES == 0) {
+				counter = 0;
+				k_yield();
+			}
 		}
 	}
 
